fd484a3c4da2222179e0381a9a316438f18c0996
isYangs <xuewuzhibu@outlook.com>
2022-10-19 16:11:19 +0800

    feat(编译选项): 02_TypeScript编译选项(2)

--- 02_编译选项/dist/app.js
+++ 02_编译选项/dist/app.js
@@ -2,3 +2,6 @@
 let a = 10;
 // console.log(hi);
 console.log(a);
+// export const hi = '你好';
+let b = 10;
+let c = 'hello';
---
+++ 02_编译选项/src/m.ts
@@ -0,0 +1,4 @@
+// export const hi = '你好';
+
+let b = 10;
+let c = 'hello';
