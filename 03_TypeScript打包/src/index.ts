2d9ed4a2fc9c5b4b501c8942eecf0625cfbf1ed1
isYangs <xuewuzhibu@outlook.com>
2022-10-21 11:17:08 +0800

    feat(typescript打包): 03_使用webpack对TypeScript打包(3)

--- 03_TypeScript打包/src/index.ts
+++ 03_TypeScript打包/src/index.ts
@@ -2,6 +2,14 @@ import { hi } from './m1';
 function sum(a: number, b: number) {
     return a + b;
 }
+const obj = { name: '张三', age: 18 };
+console.log(obj);
+
+obj.age = 20;
+console.log(obj);
+
 console.log(sum(1, 2));
 console.log('哈哈');
 console.log(hi);
+
+console.log(Promise);
